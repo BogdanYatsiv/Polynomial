11a37db583259cd77798981102fe48a99711b81e
BogdanYatsiv <bogdanyatsiv@gmail.com>
2019-11-09 17:00:07 +0200

    add pickle

--- First_prog.py
+++ First_prog.py
@@ -1,11 +1,17 @@
 from Polynomial import *
 import shelve
+import pickle
 
 cont = [(23,5),(13,1),(1,2)]
 cont2 = [(-20,5),(13,3),(1,2),(5,7),(-10,10)]
 
 a = Polinom(cont,5)
 b = Polinom(cont2,10)
+
+polynoms = [a,b]
+pickle_data = open("pickle_data.pkl", "wb")
+pickle.dump(polynoms,pickle_data)
+pickle_data.close()
 
 s = shelve.open("shelve_data.slv")
 s["A"] = a.values, a.len
--- Second_prog.py
+++ Second_prog.py
@@ -1,6 +1,8 @@
 from Polynomial import *
 import shelve
+import pickle
 
+#====SHELVE====
 s = shelve.open("shelve_data.slv")
 
 poly_a = Polinom(s["A"][0],s["A"][1])
@@ -10,7 +12,15 @@ poly_b = Polinom(s["B"][0],s["B"][1])
 print("We got poly B from shelve: ",poly_b)
 
 sum = poly_a + poly_b
-
 print("We can add them poly_a + poly_b = ", sum)
 
 s.close()
+#==============
+
+#====PICKLE====
+p = open("pickle_data.pkl","rb")
+pickle_polynoms = pickle.load(p)
+p.close()
+
+print("Sum of polynomials from pickle: ",pickle_polynoms[0] + pickle_polynoms[1])
+#==============
